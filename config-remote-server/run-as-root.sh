5c1e07bab44ba0417b96ca0d5fc767156998f9a0
Nemowang <211108100@smail.nju.edu.cn>
2024-08-01 14:32:01 +0800

    <fix>: fix typo, write sshd config under sshd_config.d/

--- config-remote-server/run-as-root.sh
+++ config-remote-server/run-as-root.sh
@@ -9,17 +9,17 @@ add-apt-repository ppa:maveonair/helix-editor -y
 apt install helix zsh python3-pip python3-pylsp curl bat tree -y
 
 # add user
-adduser --shell `which zsh` --gecos '' --disabled-password user
+adduser --shell `which zsh` --gecos '' --disabled-password $user
 
 # add sudoer
-echo -e "$user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/user
+echo -e "$user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$user
 
 # user level
-sudo -u user bash run-as-user.sh $mirror
+sudo -u $user bash run-as-user.sh $mirror
 
 # sshd config
 ufw allow ssh
 ufw allow $port/tcp
 ufw enable
-echo Port $port >> /etc/ssh/sshd_config
+echo Port $port >> /etc/ssh/sshd_config.d/${user}.conf
 systemctl restart sshd
